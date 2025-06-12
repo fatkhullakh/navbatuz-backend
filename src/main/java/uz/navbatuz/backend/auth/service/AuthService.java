105a73af7763428af757435329f34a52108826c0
fatkhullakh <tfatkh@gmail.com>
2025-06-12 23:04:03 +0200

    Finishing JWT Authentication, basic implementation of register and login with Jwts token

--- src/main/java/uz/navbatuz/backend/auth/service/AuthService.java
+++ src/main/java/uz/navbatuz/backend/auth/service/AuthService.java
@@ -1,9 +1,12 @@
 package uz.navbatuz.backend.auth.service;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import uz.navbatuz.backend.auth.dto.AuthResponse;
+import uz.navbatuz.backend.auth.dto.LoginRequest;
 import uz.navbatuz.backend.auth.dto.RegisterRequest;
 import uz.navbatuz.backend.user.model.User;
 import uz.navbatuz.backend.user.repository.UserRepository;
@@ -26,10 +29,25 @@ public class AuthService {
                 .email(request.getEmail())
                 .phoneNumber(request.getPhoneNumber())
                 .passwordHash(passwordEncoder.encode(request.getPassword()))
+                .isActive(true)
                 .build();
         userRepository.save(user);
 
         String token = jwtService.generateToken(user.getEmail());
         return new AuthResponse(token);
     }
+
+    public AuthResponse login(LoginRequest request) {
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException(request.getEmail()));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPasswordHash())) {
+            throw new BadCredentialsException("Wrong password");
+        }
+
+        String token = jwtService.generateToken(user.getEmail());
+        return new AuthResponse(token);
+    }
+
 }
+
